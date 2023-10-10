8f335521ae422fc713d074d8fa7761f4fb7506de
Thomas Buck <xythobuz@xythobuz.de>
2023-10-10 23:28:19 +0200

    rudimentary support for lipo shim

--- python-test/states.py
+++ python-test/states.py
@@ -3,6 +3,59 @@
 import uasyncio as asyncio
 import io
 import sys
+import machine
+import os
+import gc
+import time
+
+# https://github.com/pimoroni/pimoroni-pico/blob/main/micropython/examples/pico_lipo_shim/battery_pico.py
+# https://github.com/pimoroni/enviro/pull/146
+# TODO https://github.com/micropython/micropython/issues/11185
+
+full_battery = 4.2
+empty_battery = 3.2
+
+charging = machine.Pin("WL_GPIO2", machine.Pin.IN)
+conversion_factor = 3 * 3.3 / 65535
+cachedVoltage = None
+lastCaching = time.time()
+
+def set_pad(gpio, value):
+    machine.mem32[0x4001c000 | (4 + (4 * gpio))] = value
+
+def get_pad(gpio):
+    return machine.mem32[0x4001c000 | (4 + (4 * gpio))]
+
+def batteryVoltageRead():
+    vsys = machine.ADC(3)
+    voltage = vsys.read_u16() * conversion_factor
+    return voltage
+
+def batteryVoltageAverage():
+    old_pad = get_pad(29)
+    set_pad(29, 128)  # no pulls, no output, no input
+
+    sample_count = 10
+    voltage = 0
+    for i in range(0, sample_count):
+        voltage += batteryVoltageRead()
+    voltage /= sample_count
+
+    set_pad(29, old_pad)
+    return voltage
+
+def batteryVoltage():
+    global cachedVoltage, lastCaching
+
+    if ((time.time() - lastCaching) >= 2) or (cachedVoltage == None):
+        lastCaching = time.time()
+        cachedVoltage = batteryVoltageAverage()
+
+    percentage = 100.0 * ((cachedVoltage - empty_battery) / (full_battery - empty_battery))
+    if percentage > 100.0:
+        percentage = 100.0
+
+    return cachedVoltage, percentage
 
 class States:
     def __init__(self, lcd):
@@ -16,7 +69,19 @@ class States:
     async def draw(self):
         self.lcd.fill(self.lcd.black)
         self.lcd.text("Volcano Remote Control App", 0, 0, self.lcd.green)
+
         r = await self.states[self.current].draw()
+
+        voltage, percentage = batteryVoltage()
+        s = "Charging ({:.2f}V)".format(voltage)
+        c = self.lcd.green
+        if charging.value() != 1:
+            s = "{:.0f}% ({:.2f}V)".format(percentage, voltage)
+            c = self.lcd.white
+            if percentage <= 20:
+                c = self.lcd.red
+        self.lcd.text("Battery: {}".format(s), 0, self.lcd.height - 10, c)
+
         self.lcd.show()
         return r
 
@@ -90,15 +155,28 @@ lcd = LCD()
 
 # splash screen
 lcd.fill(lcd.black)
-lcd.textC("S&B Volcano Remote", int(lcd.width / 2), int(lcd.height / 2) - 25, lcd.green)
-lcd.textC("by xythobuz", int(lcd.width / 2), int(lcd.height / 2), lcd.yellow)
-lcd.textC("Initializing...", int(lcd.width / 2), int(lcd.height / 2) + 25, lcd.white)
+lcd.textC("S&B Volcano Remote", int(lcd.width / 2), 10, lcd.green)
+lcd.textC("by xythobuz",        int(lcd.width / 2), 20, lcd.yellow)
+lcd.textC("Initializing...",    int(lcd.width / 2), 30, lcd.white)
+lcd.textC(os.uname()[0][ 0 : 30], int(lcd.width / 2), lcd.height - 50, lcd.green)
+lcd.textC(os.uname()[3][ 0 : 30], int(lcd.width / 2), lcd.height - 40, lcd.yellow)
+lcd.textC(os.uname()[3][30 : 60], int(lcd.width / 2), lcd.height - 30, lcd.yellow)
+lcd.textC(os.uname()[4][ 0 : 30], int(lcd.width / 2), lcd.height - 20, lcd.white)
+lcd.textC(os.uname()[4][30 : 60], int(lcd.width / 2), lcd.height - 10, lcd.white)
 lcd.show()
 lcd.brightness(1.0)
+
+# bootloader access with face buttons
+keys = lcd.buttons()
+if keys.once("a") and keys.once("b"):
+    machine.bootloader()
 
 try:
     state_machine(lcd)
 except Exception as e:
+    sys.print_exception(e)
+
+    gc.collect()
     os = io.StringIO()
     sys.print_exception(e, os)
     s = os.getvalue()
@@ -107,4 +185,5 @@ except Exception as e:
     lcd.fill(lcd.black)
     lcd.textBlock(s, lcd.white)
     lcd.show()
+
     raise e
